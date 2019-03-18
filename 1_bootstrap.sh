2fdc3be2a28915a10fbe08ec2b07bf567de0f509
Tamas Kenez <tamaskenez@users.noreply.github.com>
2019-03-19 00:08:36 +0100

    Update 1_bootstrap.sh

--- 1_bootstrap.sh
+++ 1_bootstrap.sh
@@ -6,7 +6,7 @@ mkdir -p b
 cd b
 if [[ ! -d "buildaux" ]]; then
     echo -e "-- Cloning [buildaux]: git clone https://github.com/tamaskenez/buildaux.git b/buildaux"
-    git clone buildaux
+    git clone https://github.com/tamaskenez/buildaux.git
 else
     cd buildaux
     git pull --ff-only
