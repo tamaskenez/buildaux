5640439a9cc1a2dcc707115a57ea6be30b2dffa0
Tamas Kenez <tamaskenez@users.noreply.github.com>
2019-03-19 00:05:37 +0100

    Create 1_bootstrap.sh

---
+++ 1_bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+cd $(dirname $0)
+
+mkdir -p b
+cd b
+if [[ ! -d "buildaux" ]]; then
+    echo -e "-- Cloning [buildaux]: git clone https://github.com/tamaskenez/buildaux.git b/buildaux"
+    git clone buildaux
+else
+    cd buildaux
+    git pull --ff-only
+    cd -
+fi
